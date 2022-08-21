ad8b7648884fa8ed4eba03dd9a1f4dd9884b52f3
Samarth Saxena <samarthsaxena1672003@gmail.com>
2022-08-21 21:50:29 +0530

    let's go!

---
+++ src/Navbar.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+export default function Navbar(){
+    return(
+        <div className="navbar">
+            <i className="fa-solid fa-earth-americas"></i> Navbar rendering 
+        </div>
+    )
+}
